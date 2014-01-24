07c005e3f7013ae47936d9128a3e80bb73a19831
Rob Sanchez <rob@robsanchez.com>
2014-01-24 16:05:08 -0600

    add missing lang file

---
+++ system/expressionengine/third_party/auto_pages_uri/language/english/auto_pages_uri_lang.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+    'channels' => 'Channels',
+);
+
+/* End of file auto_pages_uri_lang.php */
+/* Location: /system/expressionengine/third_party/auto_pages_uri/language/english/auto_pages_uri_lang.php */
